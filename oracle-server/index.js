f4379b9e6a6923b816b8cf42fa2391fc5f2a1e93
equiroga8 <eduqb8@gmail.com>
2019-04-19 20:21:10 +0200

    https get request to airline api done

--- oracle-server/index.js
+++ oracle-server/index.js
@@ -16,6 +16,8 @@
 require('dotenv').config();
 const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
 var chalk = require('chalk');
+var request = require('request-promise');
+const util = require('util');
 
 class SitechainListener{
 
@@ -27,11 +29,10 @@ class SitechainListener{
 
 	init() {
 
-        	return this.bizNetworkConnection.connect('admin@delay-insurance-network')
+        	return this.bizNetworkConnection.connect('admin@insurance-network')
       		.then((result) => {
           		this.businessNetworkDefinition = result;
-              console.log(chalk.black.bgGreen.bold('Success'));
-          		//LOG.info(this.businessNetworkDefinition.getIdentifier());
+              console.log(chalk.green.bold('Connected to insurance-network successfully'));
       		})
       		// and catch any exceptions that are triggered
       		.catch(function (error) {
@@ -40,23 +41,77 @@ class SitechainListener{
 
     	}
 
-  getLHAccessToken() {
+  async getLHAccessToken() {
 
-    this.APIKey
+    var requestOptions = {
+        encoding: 'utf8',
+        uri: 'https://api.lufthansa.com/v1/oauth/token',
+        method: 'POST',
+        form: {
+            client_id : process.env.LHAPI_CLIENT_ID, 
+            client_secret: process.env.LHAPI_CLIENT_SECRET, 
+            grant_type: 'client_credentials'
+        },
+        json: true
+    };
+
+    
+    try {
+      let response = await request(requestOptions);
+      this.apiKey = response.access_token;
+      console.log(chalk.green.bold('Retrieved API key'));
+    } catch(error) {
+      console.log('ERROR: ' + error);
+    }
+  }
+
+  async getFlightStatus(flightDesignator, departureDate ) {
+
+    var getOptions = {
+          uri: 'https://api.lufthansa.com/v1/operations/flightstatus/' + flightDesignator + '/' + departureDate,
+          headers: {
+            'Authorization': 'Bearer ' + this.apiKey,
+            'Accept': 'application/json'
+          },
+          json: true 
+        };
+        try {
+          let response = await request(getOptions);
+          console.log(util.inspect(response, false, null, true));
+        } catch(error) {
+        console.log('ERROR: ' + error);
+        }
+  }
+
+   splitFlightRecordId(flightRecordId) {
+
+    var [flightDesignator] = flightRecordId.match(/[A-Z]{2}\d{3}/);
+    var unformattedDate = flightRecordId.substr(5, 8);
+    var departureDate = unformattedDate.substr(0,4) + '-' + unformattedDate.substr(4,2) + '-' + unformattedDate.substr(6);
+    console.log(flightDesignator, departureDate);
+    return { flightDesignator, departureDate }
+    
   }
 
 
 	/** Listen for the sale transaction events
      	*/
-     	listen(){
-       		this.bizNetworkConnection.on('event',(evt)=>{
+  listen(){
+    console.log(chalk.blue('Listening for OracleQuery events:'));
+    this.bizNetworkConnection.on('event',(evt)=>{
+      if (evt.flightRecordId) {
+        console.log('Oracle query event registered with flightRecordId: '+ chalk.blue(evt.flightRecordId));
 
-            if (evt.flightRecordId) {
-              console.log('Oracle query event registered with flightRecordId: '+ chalk.blue(evt.flightRecordId));
-              console.log(process.env.TEST);
-            }
-       });
-     }
+        let flightDetails = splitFlightRecordId(evt.flightRecordId);
+
+        let { flightDesignator, departureDate } = flightDetails;
+
+        let response = getFlightStatus(flightDesignator, departureDate )
+              
+      }
+      
+    });
+  }
 
 
 
@@ -67,4 +122,7 @@ class SitechainListener{
 
 var lnr = new SitechainListener();
 lnr.init();
-lnr.listen()
+lnr.getLHAccessToken();
+lnr.splitFlightRecordId('LH40420190422MAD');
+lnr.listen();
+lnr.getFlightStatus('LH404','2019-04-22' );
